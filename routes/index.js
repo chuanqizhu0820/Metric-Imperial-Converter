2b07e3aae83a25123c56b21522bed54261107cd0
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-12-17 14:29:12 +0800

    Add conversion data and log conversion statement

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+const metricToImperial = 
+{
+  km: {
+    fullName:'kilometer',
+    unit: 'mile',
+    value: 0.6214,
+  },
+  l: {
+    fullName:'liter',
+    unit: 'gallon',
+    value: 0.26417,
+  },
+  kg:{
+    fullName: "kilogram",
+    unit: 'pound',
+    value: 2.20462,
+  },
+  mi: {
+    fullName: 'mile',
+    unit: 'kilometer',
+    value: 1.60934,
+  },
+  gal: {
+    fullName: 'gallon',
+    unit: 'liter',
+    value: 3.78541,
+  },
+  lbs:{
+    unit: 'kilogram',
+    value: 0.45359,
+    fullName:'pound',
+  }
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Metric/Imperial Converter' });
@@ -18,7 +52,17 @@ router.post('/api/convert', (req, res)=>{
     number = parseFloat(numberStr);
   }
   let unit = value.match(/[a-zA-Z]/g).join('');
-  console.log(number);
   console.log(unit);
+
+  for (const item in metricToImperial){
+    if (item===unit){
+      const inputUnit = metricToImperial[item].fullName;
+      const covertedValue = number * metricToImperial[item].value;
+      const convertedUnit = metricToImperial[item].unit;
+      console.log(`${number} ${inputUnit}s converts to ${covertedValue} ${convertedUnit}s`);
+    }
+  }
+
+
 })
 module.exports = router;
