cd2a408428e963319f91b97a335a855904766d64
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-12-17 13:41:07 +0800

    Detect divison sign

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,15 @@ router.get('/', function(req, res, next) {
 
 router.post('/api/convert', (req, res)=>{
   let value = req.body.numUnit;
-  let number = parseFloat(value.match(/[0-9.]/g).join(''));
+  let numberStr = value.match(/[0-9./]/g).join('');
+  let number = null;
+  if(/[/]/.test(numberStr)){
+    let numArr = numberStr.split('/');
+    number = parseInt(numArr[0])/parseInt(numArr[1]);
+  }
+  else{
+    number = parseFloat(numberStr);
+  }
   let unit = value.match(/[a-zA-Z]/g).join('');
   console.log(number);
   console.log(unit);
